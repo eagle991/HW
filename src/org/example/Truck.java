179e0f27be19f5f47473f8bb744a44ef56b77941
eagle991 <eagle991@mail.ru>
2025-03-17 21:38:41 +0300

    ООП2

---
+++ src/org/example/Truck.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Truck extends org.example.MotorTransport {
+
+    public Truck(String modelName, int wheelCount) {
+        super(modelName, wheelCount);
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public void service () {
+        super.service();
+        checkTrailer();
+    }
+}
